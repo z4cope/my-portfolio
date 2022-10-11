88ee9ee07c897bc6be02ef0f98c9132d5a71649e
z4cope <63551733+z4cope@users.noreply.github.com>
2022-10-11 12:06:05 +0200

    Generate each card details object

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
-const closeMenuBtn = document.getElementById('close-menu-btn');
-const navBar = document.getElementById('mobile_navbar');
-const toggleBtn = document.getElementById('toggle');
-let mobNavItems = document.getElementsByClassName('mob-nav-item');
+const closeMenuBtn = document.getElementById("close-menu-btn");
+const navBar = document.getElementById("mobile_navbar");
+const toggleBtn = document.getElementById("toggle");
+let mobNavItems = document.getElementsByClassName("mob-nav-item");
 mobNavItems = Array.prototype.slice.call(mobNavItems);
+const projectCards = document.querySelectorAll(".project-card");
+const cards = [];
 
-toggleBtn.addEventListener('click', () => {
-  navBar.style.transform = 'translateX(0)';
+for (let i = 0; i < projectCards.length; i++) {
+  const cardObject = {
+    name: projectCards[i].childNodes[5].childNodes[1],
+    projectDetails: projectCards[i].childNodes[5].childNodes[3],
+    description: projectCards[i].childNodes[5].childNodes[5],
+    desktopImage: projectCards[i].childNodes[3],
+    mobileImage: projectCards[i].childNodes[1],
+    techs: projectCards[i].childNodes[5].childNodes[7],
+    liveVersion: "https://z4cope.github.io/Mobile-portfolio/",
+    srcLink: "https://github.com/z4cope/Mobile-portfolio",
+  };
+  console.log(projectCards[i].childNodes[5].childNodes[3]);
+  cards.push(cardObject);
+}
+
+console.log(cards);
+
+toggleBtn.addEventListener("click", () => {
+  navBar.style.transform = "translateX(0)";
 });
 
-closeMenuBtn.addEventListener('click', () => {
-  navBar.style.transform = 'translateX(-100%)';
+closeMenuBtn.addEventListener("click", () => {
+  navBar.style.transform = "translateX(-100%)";
 });
 
 mobNavItems.forEach((mobNavItem) => {
-  mobNavItem.addEventListener('click', () => {
-    navBar.style.transform = 'translateX(-100%)';
+  mobNavItem.addEventListener("click", () => {
+    navBar.style.transform = "translateX(-100%)";
   });
 });
